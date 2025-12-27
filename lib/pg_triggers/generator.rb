3a5c992a6185d493a30e71dce83c7e2da6c41c15
Aswin <>
2025-12-27 17:27:13 +0530

    removed the old reference

--- lib/pg_triggers/generator.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module PgTriggers
-  module Generator
-    autoload :Service, "pg_sql_triggers/generator/service"
-    autoload :Form, "pg_sql_triggers/generator/form"
-  end
-end
